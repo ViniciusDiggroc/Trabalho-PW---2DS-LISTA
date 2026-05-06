93570ba6a11e3c0ae60922969ff22f050c0dcaab
Cris <cristian.marcheti@aluno.cps.sp.gov.br>
2026-05-06 14:07:09 -0300

    Firebase Lista 2DS

--- crud-firebase/js/categoria.js
+++ crud-firebase/js/categoria.js
@@ -1,21 +1,57 @@
-const ref = db.ref("clientes");
+const ref = db.ref("categoria");
  
 $("#salvar").click(function () {
     let categoria = $("#categoria").val();
-    let info = $("#info").val();
+    let informacaos = $("#info").val();
  
-    if(categoria === "" || info === "") {
+    if (categoria === "" || informacaos === "") {
         alert("Preencha todos os campos!");
         return;
     }
  
-    ref .push({categoria, info})
-   
+    ref.push({ categoria, informacaos });
+ 
     limpar();
+});
+ 
+ref.on("value", dados_tabela => {
+    $("#lista").empty();
+ 
+    $("#lista").append(`
+        <tr>
+            <th>ID</th>
+            <th>Categoria</th>
+            <th>Informações</th>
+            <th colspan="2">Opções</th>
+        </tr>
+    `);
+   
+    dados_tabela.forEach(registro => {
+        let reg = registro.val();
+        let id = registro.key;
+ 
+        $("#lista").append(`
+            <tr>
+                <td>${id}</td>
+                <td>${reg.categoria}</td>
+                <td>${reg.informacaos}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi bi-trash"></i>
+                    </button>
+                </td>
+                <td>
+                    <button class="btn btn-warning btn-sm">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+    });
 });
  
 function limpar() {
     $("#categoria").val("");
     $("#info").val("");
     $("#categoria").focus();
-};
+}
--- crud-firebase/js/fornecedor.js
+++ crud-firebase/js/fornecedor.js
@@ -1,4 +1,4 @@
-const ref = db.ref("Fornecedor");
+const ref = db.ref("fornecedor");
 
     $("#cnpj").mask("00.000.000/0000-00");
 
@@ -16,6 +16,49 @@ $("#salvar").click(function () {
     ref.push({ nome, email, cnpj, estado });
 
     limpar();
+
+});
+
+ref.on("value", dados_tabela => {
+    $("#lista").empty();
+
+  $("#lista").append(`
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>CNPJ</th>
+            <th>estado</th>
+            <th colspan="2">Opções</th>
+        </tr>
+    `);
+
+    dados_tabela.forEach(registro => {
+        let reg = registro.val();
+        let id = registro.key;
+
+        $("#lista").append(`
+            <tr> 
+                <td>${id}</td>
+                <td>${reg.nome}</td>
+                <td>${reg.email}</td>
+                <td>${reg.cnpj}</td>
+                <td>${reg.estado}</td>
+               <td>
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi bi-trash"></i>
+                    </button>
+                </td>
+                <td>
+                    <button class="btn btn-warning btn-sm">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                </td>
+            </tr>
+            
+            `);
+    })
+
 
 });
 
